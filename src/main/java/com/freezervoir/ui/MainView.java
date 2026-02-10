9dffdd3fcb4e49885cb03bea95cbd94f41eaddf0
Helen Evans <helenijevans@gmail.com>
2026-02-10 16:56:35 +0000

    Add e2e delete functionality

--- src/main/java/com/freezervoir/ui/MainView.java
+++ src/main/java/com/freezervoir/ui/MainView.java
@@ -4,8 +4,12 @@ import com.freezervoir.entity.FreezerItems;
 import com.freezervoir.repository.FreezerItemsRepository;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.confirmdialog.ConfirmDialog;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -36,6 +40,32 @@ public class MainView extends AppLayout {
         grid.addColumn(FreezerItems::getItemId).setHeader("ID").setAutoWidth(true);
         grid.addColumn(FreezerItems::getDateAdded).setHeader("Date Added").setAutoWidth(true);
         grid.addColumn(FreezerItems::getNotes).setHeader("Notes").setAutoWidth(true);
+
+        grid.addComponentColumn(item -> {
+            Button delete = new Button(VaadinIcon.TRASH.create());
+            delete.addThemeVariants(ButtonVariant.LUMO_ERROR, ButtonVariant.LUMO_TERTIARY);
+
+            delete.addClickListener(e -> {
+                ConfirmDialog dialog = new ConfirmDialog();
+                dialog.setHeader("Delete item?");
+                dialog.setText("This action cannot be undone.");
+                dialog.setConfirmText("Delete");
+                dialog.setConfirmButtonTheme("error primary");
+                dialog.setCancelText("Cancel");
+
+                dialog.addConfirmListener(ev -> {
+                    repository.delete(item);
+                    grid.setItems(repository.findAll());
+                });
+                dialog.setCancelable(true);
+                dialog.setCloseOnEsc(true);
+
+                dialog.open();
+            });
+
+
+            return delete;
+        }).setHeader("").setAutoWidth(true);
 
         grid.setItems(repository.findAll());
         grid.setSizeFull();
